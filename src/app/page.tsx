6cf9f018e56b6df1a8124af22fd9530723f1c908
Guilherme Ananias <hi@noghartt.dev>
2023-09-17 17:42:35 -0300

    feat(rss): add initial form to add a new RSS feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { Container, Flex } from '@radix-ui/themes';
+
+import { FeedList } from './Feed/FeedList';
+import { ButtonFeedAdd } from './ButtonFeedAdd';
+
+export default async function Home() {
   return (
-    <h1>Zeit</h1>
-  );
+    <Container size='4' style={{ padding: 'var(--space-8) 0' }}>
+      <Flex gap='4'>
+        <FeedList />
+        <Flex direction='column' width='100%'>
+          <Flex justify='end' width='100%'>
+            <ButtonFeedAdd />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Container>
+  )
 }
