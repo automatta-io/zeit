4146dfa05ea21b2cfc0107e353c8124a1483cdd0
Guilherme Ananias <hi@noghartt.dev>
2023-10-03 21:59:57 -0300

    feat(rss): list feeds

--- src/app/home/Feed/CardFeedItem.tsx
+++ src/app/home/Feed/CardFeedItem.tsx
@@ -1,10 +1,28 @@
 import { Flex, Avatar, Heading } from '@radix-ui/themes';
 
-export const CardFeedItem = () => {
+type CardFeedItemProps = {
+  title: string;
+}
+
+export const getInitials = (name: string) => {
+  let _name = name.trim()
+
+  if (_name.length <= 2) {
+    return name
+  }
+
+  return _name
+      .split(/\s+/)
+      .map(w => [...w][0])
+      .slice(0, 2)
+      .join('')
+}
+
+export const CardFeedItem = ({ title }: CardFeedItemProps) => {
   return (
     <Flex gap='2' align='center'>
-      <Avatar size='1' fallback='GE' />
-      <Heading size='1'>My custom feed</Heading>
+      <Avatar size='1' fallback={getInitials(title)} />
+      <Heading size='1'>{title}</Heading>
     </Flex>
   );
 }
--- src/app/home/Feed/FeedList.tsx
+++ src/app/home/Feed/FeedList.tsx
@@ -1,16 +1,47 @@
-'use client';
-
 import { Section, Flex } from "@radix-ui/themes"
-import { CardFeedItem } from "./CardFeedItem"
-import { useLocalStorage } from "../../useLocalStorage"
+import { getServerSession } from "next-auth";
+import Parser from 'rss-parser';
 
-export const FeedList = () => {
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { db } from '../../db/db';
+import { feeds } from '../../db/schema';
+import { CardFeedItem } from "./CardFeedItem"
+
+const parser = new Parser();
+
+export const FeedList = async () => {
+  const session = await getServerSession(authOptions);
+
+  const getFeeds = async () => {
+    const feeds = await db.query.feeds.findMany({
+      with: {
+        users: {
+          where: (users, { eq }) => eq(users.email, session?.user?.email),
+        },
+      },
+    });
+
+    return Promise.all(feeds.map(async feed => {
+      const rss = await parser.parseURL(feed.url);
+
+      return {
+        ...feed,
+        title: feed.title || rss.title,
+      }
+    }));
+  }
+
+  const feeds = await getFeeds();
+
   return (
     <Section size='1' p='0' style={{ height: '100vh' }}>
       <Flex direction='column' gap='4'>
-        <CardFeedItem />
-        <CardFeedItem />
-        <CardFeedItem />
+        {feeds.map(feed => (
+          <CardFeedItem
+            key={feed.id}
+            title={feed.title}
+          />
+        ))}
       </Flex>
     </Section>
   )
